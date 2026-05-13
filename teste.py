72dcb22310b27903aefe1e8515a4713c498777fe
mairaluany <maira.luany@academico.ifpb.edu.br>
2026-05-13 12:17:41 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,89 @@
 import streamlit as st
+import base64
 
-st.image("foto-Maíra.jpeg")
-st.write("Site Maíra")
-st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/maira/in%C3%ADcio")
+# CONFIG
+st.set_page_config(page_title="Perfil", layout="wide")
+
+# FUNÇÃO base64
+def get_base64_image(path):
+    with open(path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+img_base64 = get_base64_image("star.png")
+zap_base64 = get_base64_image("zap2026.png")
+
+# TOPO (imagem clicável)
+col1, col2, col3 = st.columns([1,2,1])
+
+with col2:
+    st.markdown(f"""
+        <div style="text-align: center; margin-bottom: 50px;">
+            <a href="https://starlink.com/" target="_blank">
+                <img src="data:image/png;base64,{img_base64}" 
+                     width="320" 
+                     style="border-radius:12px;">
+            </a>
+        </div>
+    """, unsafe_allow_html=True)
+
+# LAYOUT PRINCIPAL
+col_left, col_right = st.columns([3,1])
+
+with col_left:
+    st.markdown("""
+    <div style='margin-bottom:30px; font-size:30px;'>
+        <b>Nome Dinaldo Jorge</b>
+    </div>
+    """, unsafe_allow_html=True)
+
+    # subcolunas
+    subcol1, subcol2 = st.columns([1,4])
+
+    # IMAGEM (centralizada verticalmente)
+    with subcol1:
+        st.markdown("""
+        <div style="
+            display: flex;
+            align-items: center;
+            height: 100%;
+        ">
+        """, unsafe_allow_html=True)
+
+        st.image("dino2026.png", width=800)
+
+        st.markdown("</div>", unsafe_allow_html=True)
+
+    # TEXTO
+    with subcol2:
+        st.markdown("""
+        <div style="
+            text-align: justify;
+            font-size: 20px;
+            line-height: 2.0;
+            width: 100%;
+            max-width: none;
+        ">
+            <b>Sobre Maíra:<br>
+            Maíra Luany, natural deJoão Pessoa - PB, tem 17 anos.
+            Estuda no IFPB Campus Itabaiana, cursa Informática. No seu
+            tempo livre, gost de ficar com amigos e família, tem como
+            hobby ir à academia ou praticar algum exercício físico. Além disso, 
+            tem uma fé inabalável.
+        </div>
+        """, unsafe_allow_html=True)
+
+    st.markdown("<div style='margin-top:30px;'>", unsafe_allow_html=True)
+    st.link_button("Acessar", "http://lattes.cnpq.br/4494611683890258")
+    st.markdown("</div>", unsafe_allow_html=True)
+
+with col_right:
+    st.empty()
+
+# 🔥 NOVO BLOCO (WhatsApp clicável no final)
+st.markdown(f"""
+    <div style="text-align: center; margin-top: 10px;">
+        <a href="https://wa.me/5583998234415" target="_blank">
+            <img src="data:image/png;base64,{zap_base64}" width="100">
+        </a>
+    </div>
+""", unsafe_allow_html=True)
